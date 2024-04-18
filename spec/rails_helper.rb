6357d8cbf9776c643b409fc4aac9bf63f3cabffd
Lais Almeida <laisdev25@gmail.com>
2024-04-18 03:58:37 -0300

    Adiciona o Logout (faltou no commit passado rs)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,7 +33,7 @@ RSpec.configure do |config|
   config.before(type: :system) do
     driven_by(:rack_test)
   end
-  # config.formatter = :documentation
+  config.formatter = :documentation
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
--- spec/system/buffet_admin_starts_app_spec.rb
+++ spec/system/buffet_admin_starts_app_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "User opens the app" do
+describe "BuffetAdmin opens the app" do
   it "and is not logged in" do
     #arrange
 
@@ -9,7 +9,7 @@ describe "User opens the app" do
 
     #assert
     expect(page).to have_content 'Cadê Buffet?'
-    within('header') do
+    within 'header' do
       expect(page).to have_link 'Entrar'
     end
   end
@@ -24,12 +24,12 @@ describe "User opens the app" do
 
     #act
     visit root_path
-    within('header') do
+    within 'header'  do
       click_on 'Entrar'
     end
     fill_in 'E-mail', with: admin.email
     fill_in 'Senha', with: admin.password
-    within('form') do
+    within 'form' do
       click_on 'Entrar'
     end
 
@@ -44,7 +44,8 @@ describe "User opens the app" do
       email: "buffet@adm.com",
       password: "04dm1n"
     )
-    login_as(admin, scope: :buffet_admin)
+
+    login_as admin, scope: :buffet_admin
 
     #act
     visit root_path
@@ -53,5 +54,27 @@ describe "User opens the app" do
     expect(page).to have_content "Olá, #{admin.name}"
     expect(page).to have_button "Sair"
 
+  end
+
+  it 'and logs out successfully' do
+    #arrange
+    admin = BuffetAdmin.create!(
+      name: "Ademir do Buffet",
+      email: "buffet@adm.com",
+      password: "04dm1n"
+    )
+
+    login_as admin, scope: :buffet_admin
+
+    #act
+    visit root_path
+    within 'header'  do
+      click_on 'Sair'
+    end
+
+    #assert
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Logout efetuado com sucesso.'
+    expect(page).to have_link 'Entrar'
   end
 end
